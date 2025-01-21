de4513187135cba5fcf0ae400668ceeba4cf1e29
Katsiaryna Sharstuk <Katsiaryna.Sharstuk@ventionteams.com>
2025-01-21 18:19:14 +0100

    hw1-5 done

---
+++ Library/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace Library
+{
+    public class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public string position;
+
+        public Employee(string name, string lastName, int age, string position)
+        {
+            this.firstName = name;
+            this.lastName = lastName;
+            this.age = age;
+            this.position = position;
+        }
+
+        public void EmployeeInfoPrint() => Console.WriteLine($"Name: {firstName}, LastName: {lastName}, age: {age}, Position: {position}");
+    }
+}
