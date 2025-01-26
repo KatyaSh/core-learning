9a62f032557ee09ff1c7e75464c68ab075a785bf
Katsiaryna Sharstuk <Katsiaryna.Sharstuk@ventionteams.com>
2025-01-26 23:09:28 +0100

    HW6

--- Library/Employee.cs
+++ Library/Employee.cs
@@ -1,20 +1,45 @@
 ﻿namespace Library
 {
-    public class Employee
+    public class Person
     {
-        public string firstName;
-        public string lastName;
-        public int age;
-        public string position;
-
-        public Employee(string name, string lastName, int age, string position)
+        private string firstName;
+        public string FirstName
         {
-            this.firstName = name;
-            this.lastName = lastName;
-            this.age = age;
-            this.position = position;
+            get { return firstName; }
+            set { firstName = value; }
         }
 
-        public void EmployeeInfoPrint() => Console.WriteLine($"Name: {firstName}, LastName: {lastName}, age: {age}, Position: {position}");
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        private string age;
+        public int Age { get; set; }
+
+        public virtual void PrintInfo() => Console.WriteLine($"Name: {FirstName}, LastName: {LastName}, Age: {Age}");
+    }
+
+    public class Employee : Person
+    {
+
+        private string position;
+        public string Position { get; set; }
+
+        public Employee(string firstName, string lastName, int age, string position)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Position = position;
+        }
+
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"Position: {Position}");
+        }
     }
 }
