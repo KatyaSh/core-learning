ee5416855782946a8145f3d4f20d8d2245b9f92f
Katsiaryna Sharstuk <Katsiaryna.Sharstuk@ventionteams.com>
2025-01-27 19:44:08 +0100

    task3.2 is done

--- Program.cs
+++ Program.cs
@@ -40,12 +40,12 @@
 // Task 3-3
 {
     Console.Write("Enter your number:");
-    var userNumber = Console.ReadLine();
-    bool isParsedResult = int.TryParse(userNumber, out int parsedNumber);
+    var userNumberSpec = Console.ReadLine();
+    bool isParsedResult = int.TryParse(userNumberSpec, out int parsedNumberSpec);
 
     if (isParsedResult)
     {
-        int result = parsedNumber + parsedNumber * 2;
+        int result = parsedNumberSpec + parsedNumberSpec * 2;
         Console.WriteLine("Your result is: " + result);
     }
     else
@@ -69,14 +69,14 @@
 // Task 3-5
 {
     Console.Write("Enter your number:");
-    var userNumber = Console.ReadLine();
-    bool isParsedNumber = int.TryParse(userNumber, out int parsedNumber);
+    var userNumberSpecif = Console.ReadLine();
+    bool isParsedNumber = int.TryParse(userNumberSpecif, out int parsedNumberSpecif);
 
-    if (isParsedNumber && Math.Abs(parsedNumber) % 2 == 0)
+    if (isParsedNumber && Math.Abs(parsedNumberSpecif) % 2 == 0)
     {
         Console.WriteLine("Your number is: Even");
     }
-    else if (isParsedNumber && Math.Abs(parsedNumber) % 2 == 1)
+    else if (isParsedNumber && Math.Abs(parsedNumberSpecif) % 2 == 1)
     {
         Console.WriteLine("Your number is: Odd");
     }
@@ -353,6 +353,68 @@
     }
 }
 
+//Task3-2-1
+Console.WriteLine("enter two numbers and one of the following arithmetic operations - +, -, *, / using space between THEM: 9 + 9, 8 - 4... ");
+var userEnter = Console.ReadLine();
+
+string[] parts = userEnter.Split(" ");
+
+var isFirstNumber = double.TryParse(parts[0], out double num1);
+var isSecondNumber = double.TryParse(parts[2], out double num2);
+
+switch (parts[1])
+{
+    case "-":
+        Console.WriteLine($"{num1 - num2}");
+        break;
+    case "+":
+        Console.WriteLine($"{num1 + num2}");
+        break;
+    case "*":
+        Console.WriteLine($"{num1 * num2}");
+        break;
+    case "/":
+        if (num2 != 0)
+            Console.WriteLine($"{num1 / num2}");
+        else
+            Console.WriteLine("dividing by zero");
+        break;
+    default:
+        Console.WriteLine("Operation is not present in the list of allowed operations");
+        break;
+}
+
+//Task3-2-2
+Console.WriteLine("enter a number: ");
+var userNumber = Console.ReadLine();
+bool specifiedNumber = int.TryParse((userNumber), out int a1);
+if (specifiedNumber)
+{
+    if (a1 < 50 && a1 != 37 && a1 >= 32)
+    {
+        Console.WriteLine("Working!");
+    }
+    else if (a1 == 0 || a1 == 15)
+    {
+        Console.WriteLine("Working!");
+    }
+    else
+    {
+        Console.WriteLine("Does’t work");
+    }
+}
+else
+{
+    Console.WriteLine("You specified not a number");
+}
+
+//Task3-2-3
+Console.WriteLine("enter a number: ");
+var enteredNumber = Console.ReadLine();
+bool parsedNumber = int.TryParse((enteredNumber), out int parsNum);
+
+Console.WriteLine((parsedNumber && parsNum % 2 == 0) ? $"{parsNum} is Even" : $"{parsNum} is Odd");
+
 var rabotnik = new Employee("Pasha", "Bubkin", 23, "gruzchik");
 var pracauniki = new Employee[] { rabotnik, new Employee("Venus", "Papus", 45, "director"), new Employee("Mada", "Petrovna", 56, "buhgalter") };
 var factory = new Factory("Rassvet");
@@ -422,7 +484,6 @@ class Factory
         }
     }
 
-
     public void PrintEmployeesInfoCertainPositin(string position)
     {
         foreach (Employee employee in employees)
@@ -434,5 +495,3 @@ class Factory
         }
     }
 }
-
-
